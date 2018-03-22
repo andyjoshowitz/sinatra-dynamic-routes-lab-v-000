763575d0d2a35bc5626e0b3a06ea39e16c580b14
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-22 14:50:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,9 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase] 
-    @phrse * @number
+    @phrase * @number
   end
+  
+  get 
 
 end
