886be8111ad364b5a07f34017f947a9b0e34855c
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-22 14:56:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,5 +25,11 @@ class App < Sinatra::Base
     @words = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]].join(" ")
     "#{@words}."
   end
-
+  
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
+    @number1 = [:number1]
+    @number2 = [:number2]
+    
+  end
 end
